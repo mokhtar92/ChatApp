b266600c8377077981e0bc2b9ebd75b9b834479f
ahmed-mokhtar <31440751+ahmed-mokhtar@users.noreply.github.com>
2018-02-07 10:55:48 +0200

    Adding full Structure
    @hanaamaldaly  @ahmed-maroof  @ghadamohmed

---
+++ server/src/controller/FXMLServerScreenController.java
@@ -0,0 +1,36 @@
+package controller;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author Ahmed_Mokhtar
+ */
+public class FXMLServerScreenController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
